71b8b771f63469ff4ad9e6ffdba2ebac464bf12b
lukechidubem <lukechidubem@gmail.com>
2022-11-23 14:10:06 +0100

    modify product

--- src/components/ProductsHome.js
+++ src/components/ProductsHome.js
@@ -19,26 +19,25 @@ const reducer = (state, action) => {
 };
 
 const ProductsHome = () => {
-  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+  // const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+  const [{ loading, error }, dispatch] = useReducer(logger(reducer), {
     products: [],
     loading: true,
     error: "",
   });
 
-  // const [products, setProducts] = useState([]); //default is empty database or json, no products
+  const [products, setProducts] = useState([]); //default is empty database or json, no products
 
   useEffect(() => {
     const fetchData = async () => {
       dispatch({ type: "FETCH_REQUEST" });
       try {
-        const result = await axios.get(
-          "https://lukescommerce.onrender.com/api/products"
-        );
+        const result = await axios.get("/api/products");
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (err) {
         dispatch({ type: "FETCH_FAIL", payload: err.message });
       }
-      //   //setProducts(result.data);
+      setProducts(result.data);
     };
     fetchData();
   }, []);
